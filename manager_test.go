825f63772c8c276f253bc426b0b5641eed0742b1
lixianmin <lixianmin@live.cn>
2020-12-01 14:29:06 +0800

    合并测试代码到manager_test.go中

--- manager_test.go
+++ manager_test.go
@@ -13,6 +13,17 @@ author:     lixianmin
 Copyright (C) - All Rights Reserved
 *********************************************************************/
 
+type TestConfig struct {
+	Notice string  `xlsx:"notice"`
+	Tips   *string `xlsx:"tips"`
+}
+
+type FakeConfig struct {
+	ID   int
+	Name string `xlsx:"name"`
+}
+
+
 type TestPerson struct {
 	Name string
 	Age  int
@@ -106,4 +117,17 @@ func TestManager_GetTemplateByIntXX(t *testing.T) {
 	assert.True(t, manager.GetTemplate(&template, uint64(1)))
 	assert.True(t, manager.GetTemplate(&template, int(1)))
 	assert.True(t, manager.GetTemplate(&template, uint(1)))
+}
+
+func TestManager_GetConfig(t *testing.T) {
+	var manager = &Manager{}
+	manager.AddExcel(ExcelArgs{FilePath: testExcelFilePath})
+
+	var config TestConfig
+	assert.True(t, manager.GetConfig(&config))
+	assert.True(t, manager.GetConfig(&config, WithSheetName("TestConfig")))
+
+	var fake FakeConfig
+	assert.False(t, manager.GetConfig(&fake))
+	assert.False(t, manager.GetConfig(&fake, WithSheetName("FakeConfig")))
 }
